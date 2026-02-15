13dafcb71162c21572e2a5cf5e4f13e7cd90f37c
somerandomguythatneedshelp <107365137+somerandomguythatneedshelp@users.noreply.github.com>
2026-02-15 03:27:43 +0000

    ext: added more duas/dhikr and translations for all of them

--- src/lib/Dhikrs.ts
+++ src/lib/Dhikrs.ts
@@ -2,7 +2,11 @@ enum DhikrType {
   MORNING = '0',
   EVENING = '1',
   EATING = '2',
-  POSTPRAYER = '3'
+  POSTPRAYER = '3',
+  IMANPROTECTION = '4',
+  BEFORESLEEP = '5',
+  NIGHTMARES = '6',
+  DEATH = '7'
 }
 
 export { DhikrType };
